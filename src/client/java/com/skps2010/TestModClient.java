ab133aa86c3d968cdfbd37ce0e84a6e641d9670c
skps2010 <skps2010030908@yahoo.com>
2025-09-18 18:05:54 +0800

    init

---
+++ src/client/java/com/skps2010/TestModClient.java
@@ -0,0 +1,10 @@
+package com.skps2010;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TestModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}

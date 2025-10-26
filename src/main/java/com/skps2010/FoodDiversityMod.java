16a08a020c947873e03ede9f594b3bb0b4bb276c
skps2010 <skps2010030908@yahoo.com>
2025-10-26 17:54:21 +0800

    smallest working example

--- src/main/java/com/skps2010/FoodDiversityMod.java
+++ src/main/java/com/skps2010/FoodDiversityMod.java
@@ -1,18 +1,10 @@
 package com.skps2010;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.event.player.UseItemCallback;
-import net.fabricmc.fabric.api.client.item.v1.ItemTooltipCallback;
 
 public class FoodDiversityMod implements ModInitializer {
-    public static final String MODID = "fooddiversity";
-
     @Override
     public void onInitialize() {
-        System.out.println("[FoodDiversity] Mod initialized!");
-
-        // 註冊事件 (後續會實作)
-        UseItemCallback.EVENT.register(FoodEventHandler::onUseItem);
-        ItemTooltipCallback.EVENT.register(FoodTooltipHandler::onTooltip);
+        System.out.println("[FoodDiversityMod] Initialized - all foods now restore 6 hunger!");
     }
 }
